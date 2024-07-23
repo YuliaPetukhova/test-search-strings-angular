1a45ebf01d3aae9e4d4f6682ae27b7b1a9b816c6
Petukhova Yulia <petukhova.yulia99@gmail.com>
2024-07-23 15:05:43 +0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {MatAccordion, MatExpansionPanel, MatExpansionPanelHeader} from "@angular/material/expansion";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, MatAccordion, MatExpansionPanel,MatExpansionPanelHeader],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
